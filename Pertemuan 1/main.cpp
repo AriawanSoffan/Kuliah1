a868aaf8f04590edbea438f5287445a05e669489
AriawanSoffan <112879921+AriawanSoffan@users.noreply.github.com>
2022-09-05 21:58:27 +0700

    Add files via upload

---
+++ Pertemuan 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Ariawan Soffan Farajaya" << endl;
+    cout << "NIM : A11.2022.14112" << endl;
+    cout << "Alamat : Jl. Wisma Prasetya IV no IX" << endl;
+    cout << "Asal Sekolah : SMA ISLAM SULTAN AGUNG 1 SEMARANG" << endl;
+    return 0;
+}
