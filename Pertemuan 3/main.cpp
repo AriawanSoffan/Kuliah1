abc629a1b7f90eee8a1b55889c5d111deaa67799
AriawanSoffan <112879921+AriawanSoffan@users.noreply.github.com>
2022-09-12 22:29:55 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int a = 18;
+    int b = 17;
+    float c = 175.5;
+    char d = 'L';
+    string e = "dilarang buang sampah sembarangan" ;
+
+    cout << "Nama : Ariawan Soffan Farajaya" << endl;
+    cout << "NIM : A11.2022.14112" << endl;
+    cout << "Alamat : Jl. Wisma Prasetya IV no IX" << endl;
+    cout << "Asal Sekolah : SMA ISLAM SULTAN AGUNG 1 SEMARANG" << endl;
+
+    cout<<"\nUmur : "<<a;
+    cout<<"\nUmur kemarin : "<<b;
+    cout<<"\nTinggi Badan : "<<c;
+    cout<<"\nKelamin : "<<d;
+    cout<<"\nPesan : "<<e;
+
+
+    return 0;
+}
