bcc633574b875c864f7ab90467205175f6dfba91
AriawanSoffan <112879921+AriawanSoffan@users.noreply.github.com>
2022-09-19 22:25:40 +0700

    Add files via upload

---
+++ Pertemuan 5/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    float y;
+    float z;
+    int opsi;
+    menu:
+    cout<< "Pilih soal yang akan dipecahkan"<<endl;
+    cout<< "Luas lingkaran ketik 1"<<endl;
+    cout<< "Luas persegi panjang ketik 2"<<endl;
+    cout<< "Untuk keluar ketik 3"<<endl<<endl;
+
+    cout<< "Pilih : " ;
+    cin>>opsi;
+    if(opsi==1){
+        cout <<"Jari Jari : ";
+        cin >> x;
+        cout <<"Luas Lingkaran : " << x*x*22/7<<endl<<endl;
+        goto menu;
+    }
+    else if(opsi==2){
+        cout <<"Panjang : ";
+        cin >> y;
+        cout<<"luas : ";
+        cin >> z;
+        cout<<"Luas Persegi Panjang : " <<y*z<<endl<<endl;
+        goto menu;
+    }
+        else if(opsi==3){
+        }
+        else {
+        cout<<"pilihannya 1,2 dan 3 doang bos, pilih lagi"<<endl<<endl;
+        goto menu;
+        }
+
+    return 0;
+}
