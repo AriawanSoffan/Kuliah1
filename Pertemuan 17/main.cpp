f125cc9dc1ca091c98b076d5cf68021fec09551f
AriawanSoffan <112879921+AriawanSoffan@users.noreply.github.com>
2022-11-23 12:07:30 +0700

    Create main.cpp

---
+++ Pertemuan 17/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 100 , j = 1 ; i>=0 , j<=100 ; i-- , j++){
+
+             cout<< "i=" << i << " dan j=" << j << endl;
+            }
+
+    return 0;
+}
